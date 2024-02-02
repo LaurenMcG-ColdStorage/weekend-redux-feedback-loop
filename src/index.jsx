dccd6f58686f93fe48711fba7ef689d6e4ca9fd3
Lauren McGarvey <l.al.mcgarvey@gmail.com>
2024-02-01 21:04:26 -0500

    Initial setup

--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 
+import { store } from './store';
+import { Provider } from 'react-redux';
+
+const appStore = store() 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={appStore}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
