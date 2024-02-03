910e2c7080ed2c6dc7faeb950d67d316fd0f6f0d
Lauren McGarvey <l.al.mcgarvey@gmail.com>
2024-02-03 16:50:31 -0500

    dispatch and local state for understanding.jsx created

--- src/components/Understanding/Understanding.jsx
+++ src/components/Understanding/Understanding.jsx
@@ -1,5 +1,19 @@
+import { useDispatch } from "react-redux";
+import { useState } from 'react';
+
 function UnderstandingLevel(){
 
+    const [understandingValue, setUnderstandingValue] = useState(3);
+
+    const submitUnderstanding = (event) => {
+        event.preventDefault();
+        const dispatch = useDispatch();
+        const action = {
+            type: "CURRENT_UNDERSTANDING",
+            payload: understandingValue
+        };
+        dispatch(action);
+    };
 }
 
 export default UnderstandingLevel;
