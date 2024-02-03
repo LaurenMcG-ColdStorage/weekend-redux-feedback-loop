b395553481f0ce55d7cedfd5c9a8e1b0934f7eea
Lauren McGarvey <l.al.mcgarvey@gmail.com>
2024-02-03 17:12:30 -0500

    support survey page built

--- src/components/Support/Support.jsx
+++ src/components/Support/Support.jsx
@@ -3,6 +3,36 @@ import { useState } from 'react';
 
 function SupportAmount(){
 
+        const [supportValue, setSupportValue] = useState(3);
+
+        const submitSupport = (event) => {
+            event.preventDefault();
+            const dispatch = useDispatch();
+            const action = {
+                type:"CURRENT_SUPPORT",
+                payload: supportValue
+            };
+            dispatch(action);
+        };
+
+        return(
+            <div>
+                <h2>How supported are you feeling?</h2>
+                <label>Scale of 1 to 5</label>
+                <select name="support" 
+                        className='support-style'  
+                        value={supportValue}
+                        onChange={(event) => setSupportValue(event.target.value)}
+                        required>
+                    <option value={1}>1</option>
+                    <option value={2}>2</option>
+                    <option value={3}>3</option>
+                    <option value={4}>4</option>
+                    <option value={5}>5</option>
+                </select>
+            <button onClick={(event) => submitSupport(event)}>Next</button>
+        </div>
+        )
 };
 
 export default SupportAmount;
