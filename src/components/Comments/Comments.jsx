984bd16f88910885528e654b948fe9b12eedb9cd
Lauren McGarvey <l.al.mcgarvey@gmail.com>
2024-02-03 17:36:23 -0500

    comment page built

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -3,6 +3,25 @@ import { useState } from 'react';
 
 function CommentPage(){
 
+    const [commentValue, setCommentValue] = useState('');
+    
+    const submitComment = (event) => {
+        event.preventDefault();
+        const dispatch = useDispatch();
+        const action = {
+            type: "COMMENTS",
+            payload: commentValue
+        };
+        dispatch(action);
+    };
+
+    return(
+        <div>
+            <h2>Would you like to leave any comments?</h2>
+            <input type='string' placeholder='Enter text here'></input>
+            <button onClick={(event) => submitComment(event)}>Complete</button>
+        </div>
+    );
 };
 
 export default CommentPage;
