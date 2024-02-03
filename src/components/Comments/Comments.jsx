e2b47ccd425d62e7d6ab318f9d713c970ab3b8bf
Lauren McGarvey <l.al.mcgarvey@gmail.com>
2024-02-02 21:05:28 -0500

    Created store.js reducer skeleton

---
+++ src/components/Comments/Comments.jsx
@@ -0,0 +1,5 @@
+function CommentPage(){
+
+};
+
+export default CommentPage;
--- src/store.js
+++ src/store.js
@@ -1,3 +1,40 @@
 import { createStore, combineReducers, applyMiddleware} from 'redux';
 import logger from 'redux-logger';
 
+const feeling = (state = 1, action) => {
+    if(action.type === "CURRENT_FEELING"){
+        return state = action.payload;
+    }
+    return state;
+};
+
+const understanding = (state = 1, action) => {
+    if (action.type === "CURRENT_UNDERSTANDING"){
+        return state = action.payload;
+    }
+    return state;
+};
+
+const support = (state = 1, action) => {
+    if (action.type === "CURRENT_SUPPORT"){
+        return state = action.payload;
+    }
+    return state;
+};
+
+const comments = (state = [], action) => {
+    if (action.type === "COMMENT"){
+        return [...state, action.payload];
+    }
+    return state;
+};
+
+const storeInstance = createStore(
+    combineReducers({
+        feeling,
+        understanding,
+        support,
+        comments
+    }),
+    applyMiddleware(logger)
+)
